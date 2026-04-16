c2aca152cfd426bd958849c23dcbb080584e81b5
adrianandcathysmith-arch <adrianandcathysmith@gmail.com>
2026-04-16 11:31:46 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "gs bucket id - tf-remote-state-student_04_0eaf8297f7fe-18961-30557"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
